475345381c662381a99dfad27955149d73003629
Mr Lima <felipebr4095@gmail.com>
2025-10-08 11:50:11 -0300

    Rendering main page

--- app/cardapio.py
+++ app/cardapio.py
@@ -1,42 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# |
-# Imports
-# | (Flask)
-from flask import Flask, request, jsonify, send_from_directory, make_response, render_template, redirect
+from flask import Flask, render_template, jsonify
 from flask_cors import CORS
-from flask import send_file
-# | (Others) 
-import os
-import jwt
-import time
-import json
-import uuid
-import flask
-import socket
-import random
-import bcrypt
-import shutil
-import requests
-import threading, pytz
-from random import randint
-from threading import Timer
-from threading import Thread
-from cryptography.fernet import Fernet
-from datetime import datetime, timedelta
-from werkzeug.utils import secure_filename
-from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
-# |
-# |
+from pymongo import MongoClient
+
 app = Flask(__name__)
 CORS(app)
-# |
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["bemmecake"]
+produtos = db["produtos"]
+
 @app.route("/cardapio/")
 def carregar():
-    return render_template()
-# |
-# |
-# |
-# Start API
-if __name__ == '__main__':
-    app.run(port=31523, debug=True, host="127.0.0.1")
+    bolos_caseiros = list(produtos.find({"type": "bolo"}))
+    doces = list(produtos.find({"type": "doce"}))
+    bolos_de_festa = list(produtos.find({"type": "festa"}))
+    recheios = list(produtos.find({"type": "recheio"}))
+    bolos_de_pote = list(produtos.find({"type": "pote"}))
+    novidades = list(produtos.find({"type": "novidade"}))
+    bebidas = list(produtos.find({"type": "bebida"}))
+
+    return render_template(
+        "cardapio.html",
+        bolos_caseiros=bolos_caseiros,
+        doces=doces,
+        bolos_de_festa=bolos_de_festa,
+        recheios=recheios,
+        bolos_de_pote=bolos_de_pote,
+        novidades=novidades,
+        bebidas=bebidas,
+    )
+
+if __name__ == "__main__":
+    app.run(port=31520, debug=True, host="127.0.0.1")
